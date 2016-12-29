049e77ac004ec014e5ceb16fe131253b39643e02
Marco <jesusmarcodelcarmen@gmail.com>
2016-12-29 00:51:44 -0600

    added container

--- TinderAPP/src/components/app.js
+++ TinderAPP/src/components/app.js
@@ -1,9 +1,14 @@
+'use strict';
+
 import React, { Component } from 'react';
+import BookList from '../containers/book_list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
---
+++ TinderAPP/src/containers/book_list.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="col-sm-4 list-group">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  //whatever is returned will show up as props
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
