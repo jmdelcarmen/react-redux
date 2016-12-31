55348fe9a458eb1d6c4d311ab2b8e0c5094fb8e0
Marco <jesusmarcodelcarmen@gmail.com>
2016-12-30 18:02:32 -0600

    weather app initial setup

---
+++ ReduxPrimer/src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+'use strict';
+//books reducer returns an array.
+export default function (state) {
+  return [
+    {title: 'JavaScript: The Good Parts', pages: 100},
+    {title: 'Harry Potter', pages: 205},
+    {title: 'The Dark Tower', pages: 354},
+    {title: 'Eloquent Ruby', pages: 710}
+  ]
+}
